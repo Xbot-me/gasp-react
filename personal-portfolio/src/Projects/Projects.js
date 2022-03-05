9bb988345752562a5d384104865d7e39a476bf87
Xbot-me <mustafizur15-6939@diu.edu.bd>
2022-03-06 02:47:23 +0600

    working on a portfolio using gasp and react first commit

---
+++ personal-portfolio/src/Projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Header/Header'
+import './Projects.css'
+function Projects() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Projects
